a2b29621d01ff7d826d47f68cc672e078d5346ba
mountain <Fiveoat@gmail.com>
2021-01-14 00:52:44 -0700

    asldkjf

--- web/index.php
+++ web/index.php
@@ -1,11 +1,21 @@
-<?php $root = $_SERVER['DOCUMENT_ROOT'];?>
+<?php $root = $_SERVER['DOCUMENT_ROOT']; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/main.css" media="screen">
   <script type="text/javascript" src="/main.js"></script>
+  <script>
+    function getImage() {
+      let i = 0;
+      let images = ["<img src='images/' alt='image'>", "<img src='images/' alt=image>", "<img src='images/' alt='image'>"]
+      i = Math.floor(Math.random() * images.length);
+      document.getElementById('x').innerHTML = images[i];
+    }
+  </script>
+
   <title>Home Page</title>
 </head>
 
@@ -15,13 +25,13 @@
       <?php require $root . '/header.php'; ?>
     </header>
     <nav>
-    <?php require $root . '/nav.php'; ?>
+      <?php require $root . '/nav.php'; ?>
     </nav>
     <main>
-    <h1>About Me</h1>
-    <p>I think that the </p>
-    <button onclick="getImage()">
-    <div id="x"></div>
+      <h1>About Me</h1>
+      <p>I think that the </p>
+      <button onclick="getImage()">
+        <div id="x"></div>
     </main>
     <footer>
       <?php require $root . '/footer.php'; ?>
