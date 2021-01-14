e56ba2a47f7a80997a576d68664d1aea430bde1c
mountain <Fiveoat@gmail.com>
2021-01-14 00:26:59 -0700

    asdfj

--- web/header.php
+++ web/header.php
@@ -1,2 +1,2 @@
 <?php $root = $_SERVER['DOCUMENT_ROOT'];?>
-<div class="image"><img src="/images/IMG_2675.jpg" alt="image" style="width:100%"></div>
+<div class="image"><img src="/images/IMG_2675.jpg" alt="image"></div>
