cd5a32727091c15eaddd4540eb25d4ebfb86d486
mountain <Fiveoat@gmail.com>
2021-01-14 02:48:19 -0700

    asldfj

--- web/index.php
+++ web/index.php
@@ -23,7 +23,7 @@
     <h1 class="display-1">About Me</h1>
       <div class="container">
         <div class="row">
-          <div class="col-sm">
+          <div class="col-md">
           <p>Hello. My name is Coty Fivecoat. I am 27. I live in Salt Lake City. I currently work as a Software Development Engineer at a cybersecurity start up, but we were recently aquired.
         I don't have very much experience with Web Development, other than the Python frameworks Flask & FastAPI, though it has been my job for a couple of weeks now. I actually got this job because I worked on
         the Operations team & managed to automate in the thousands of hours of various tasks between different teams in my free time with Python. I feel very comfortable with Python,
@@ -31,7 +31,7 @@
         
       </p>
           </div>
-          <div class="col-sm">
+          <div class="col-md">
             <p>I really enjoy writing code & I feel very lucky to be where I am. I was also very lucky to work in an organization
         young enough to let me have access to all production databases & admin privileges to Jira, both of which I leveraged greatly along with our internal API endpoints.
         I feel like I would try & code on my own, but would have a hard time applying things I would learn, but at work I had endless tasks from scaling our customer requests processes, to creating an auto QA process,
@@ -40,20 +40,21 @@
         C, C++, Rust, PHP, & even JavaScript though it is the next easiest. I spend the vast majority of my free time rock climbing, skiing ( backcountry, XC, & in resort ), trail running, biking, & writing code.
         </p>
           </div>
-          <div class="col-sm">
+          <div class="col-md">
             <p>I really would like to get good at backend web development & machine learning. I have recently been spending a lot of time learning about finance & investing as well. One last interesting fact about me is that I live in a built out
         Dodge ProMaster with my wife. I have very low expectations for comfort & I am extremely blessed that my wife shares that as well.</p>
           </div>
         </div>
       </div>
+      <br><br>
       <div class="row">
-          <div class="col-sm">
+          <div class="col-md">
           <img src="images2/IMG_1422.jpg" alt="image" class="rounded img-thumbnail">
           </div>
-          <div class="col-sm">
+          <div class="col-md">
           <img src="images2/IMG_1297.jpg" alt="image" class="rounded img-thumbnail">  
           </div>
-          <div class="col-sm">
+          <div class="col-md">
           <img src="images2/IMG_1173.jpg" alt="image" class="rounded img-thumbnail">  
           </div>
         </div>
