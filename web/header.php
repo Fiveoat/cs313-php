6db3a42b4e0e5a5f7a157781e5926ab84bb975af
mountain <Fiveoat@gmail.com>
2021-01-14 00:12:05 -0700

    asljdf

--- web/header.php
+++ web/header.php
@@ -1,1 +1,2 @@
-<div class="image"><img src="/web/images/yosemite.jpg" alt="image"></div>
+<?php $root = $_SERVER['DOCUMENT_ROOT'];?>
+<div class="image"><img src="web/images/yosemite.jpg" alt="image"></div>
