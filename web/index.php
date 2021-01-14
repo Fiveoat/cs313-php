2d3495a8a5d77187b0602127fbd718af41a75d7f
mountain <Fiveoat@gmail.com>
2021-01-14 01:16:13 -0700

    asf,j

--- web/index.php
+++ web/index.php
@@ -7,19 +7,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/main.css" media="screen">
   <script type="text/javascript" src="/main.js"></script>
-  <script>
-    function getImage() {
-      let i = 0;
-      let images = []
-      let image_files = ['0713-2', '0752', '2675', '2983', '3584', '3629', '9186', '9399', '9984']
-      for (i = 0; i < image_files.length; i++){
-        images.push(`<img src='images/IMG_${image_files[i]}.jpg' alt='image'>`)
-      }
-      i = Math.floor(Math.random() * images.length);
-      document.getElementById('x').innerHTML = images[i];
-    }
-  </script>
-
   <title>Home Page</title>
 </head>
 
