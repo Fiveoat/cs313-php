3b0ee1b1624c2494cfebaf543af964c32ccd6368
mountain <Fiveoat@gmail.com>
2021-01-14 01:14:11 -0700

    asdjl

--- web/index.php
+++ web/index.php
@@ -10,10 +10,10 @@
   <script>
     function getImage() {
       let i = 0;
-      let images = ['0713-2', '0752', '2675', '2983', '3584', '3629', '9186', '9399', '9984']
-      let image_files = []
+      let images = []
+      let image_files = ['0713-2', '0752', '2675', '2983', '3584', '3629', '9186', '9399', '9984']
       for (i = 0; i < image_files.length; i++){
-        images.push(`<img src='images/IMG_${image_file}.jpg' alt='image'>`)
+        images.push(`<img src='images/IMG_${image_files[i]}.jpg' alt='image'>`)
       }
       i = Math.floor(Math.random() * images.length);
       document.getElementById('x').innerHTML = images[i];
