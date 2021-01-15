8839f14b249008b0b175f01f652a004bf3bb8262
mountain <Fiveoat@gmail.com>
2021-01-14 18:17:51 -0700

    worked

---
+++ web/assignments.php
@@ -0,0 +1,31 @@
+<?php $root = $_SERVER['DOCUMENT_ROOT']; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/main.css" media="screen">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <script type="text/javascript" src="/main.js"></script>
+  <title>Home</title>
+</head>
+
+<body>
+  <div class=".container-md">
+    <header class="display-4">
+      <?php require $root . '/header.php'; ?>
+    </header>
+    <nav>
+      <?php require $root . '/nav.php'; ?>
+    </nav>
+    <main>
+    <h1 class="display-1">COMING SOON</h1>
+    </main>
+  <footer>
+    <?php require $root . '/footer.php'; ?>
+  </footer>
+  </div>
+</body>
+
+</html>
--- web/nav.php
+++ web/nav.php
@@ -3,7 +3,7 @@
     <a class="nav-link active" aria-current="page" href="#">Home&ensp;</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Assignments&ensp;</a>
+    <a class="nav-link" href="/assignments.php">Assignments&ensp;</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="#">Other&ensp;</a>
