c0a82ad01a9e29b3abad0f0ebfd0115db8db1649
mountain <Fiveoat@gmail.com>
2021-01-14 18:19:23 -0700

    worked

--- web/assignments.php
+++ web/assignments.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="/main.css" media="screen">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <script type="text/javascript" src="/main.js"></script>
-  <title>Home</title>
+  <title>Assignments</title>
 </head>
 
 <body>
