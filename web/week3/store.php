feb0ea2f59f1afc6cd93f032c17493e5c9b4d484
mountain <Fiveoat@gmail.com>
2021-01-19 23:59:14 -0700

    x

--- web/week3/store.php
+++ web/week3/store.php
@@ -93,6 +93,46 @@ $prices = array(120, 110, 100, 90, 80, 70, 60, 50, 40);
             </div>
         </div>
     </div>
+    <?php
+    if (isset($_SESSION["cart"])) {
+    ?>
+        <br /><br /><br />
+        <h2>Cart</h2>
+        <table>
+            <tr>
+                <th>Product</th>
+                <th width="10px">&nbsp;</th>
+                <th>Qty</th>
+                <th width="10px">&nbsp;</th>
+                <th>Amount</th>
+                <th width="10px">&nbsp;</th>
+                <th>Action</th>
+            </tr>
+            <?php
+            $total = 0;
+            foreach ($_SESSION["cart"] as $i) {
+            ?>
+                <tr>
+                    <td><?php echo ($products[$_SESSION["cart"][$i]]); ?></td>
+                    <td width="10px">&nbsp;</td>
+                    <td><?php echo ($_SESSION["qty"][$i]); ?></td>
+                    <td width="10px">&nbsp;</td>
+                    <td><?php echo ($_SESSION["amounts"][$i]); ?></td>
+                    <td width="10px">&nbsp;</td>
+                    <td><a href="?delete=<?php echo ($i); ?>">Delete from cart</a></td>
+                </tr>
+            <?php
+                $total = $total + $_SESSION["amounts"][$i];
+            }
+            $_SESSION["total"] = $total;
+            ?>
+            <tr>
+                <td colspan="7">Total : <?php echo ($total); ?></td>
+            </tr>
+        </table>
+    <?php
+    }
+    ?>
     <!-- <form method="POST" action="shopping_cart.php">
             <div class="container">
                 <div class="row">
