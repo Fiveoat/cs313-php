db301eae0a98b2b94b62b226485a4fd0402abf1d
mountain <Fiveoat@gmail.com>
2021-01-19 21:31:03 -0700

    x

--- web/week3/shopping_cart.php
+++ web/week3/shopping_cart.php
@@ -1,11 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Wind River Range Equipment</title>
+    <link rel="stylesheet" type="text/css" href="/week3/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
 <?php $root = $_SERVER['DOCUMENT_ROOT']; ?>
 <header class="display-4">
-	<?php require $root . '/week3/header.php'; ?>
+    <?php require $root . '/week3/header.php'; ?>
 </header>
 <nav>
-	<?php require $root . '/week3/nav.php'; ?>
-</nav>
+      <?php require $root . '/week3/nav.php'; ?>
+    </nav>
+
 
 <footer>
-	<?php require $root . '/week3/footer.php'; ?>
+    <?php require $root . '/week3/footer.php'; ?>
 </footer>
+</html>
