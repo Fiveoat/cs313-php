a6dbc7ad5209595d72278fb86880746e9b0e6d48
mountain <Fiveoat@gmail.com>
2021-01-19 21:51:35 -0700

    x

--- web/week3/shopping_cart.php
+++ web/week3/shopping_cart.php
@@ -1,44 +1,30 @@
-<?php
-foreach ($_POST as $key => $value) {
-    echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>";
-}
-
-foreach($_POST['products'] as $value)
-{
-	echo 'Been To: '. $value. '<br>';
-}
-echo $_POST['products'];
-// $firstName = $_POST["first_name"];
-// $lastName = $_GET["last_name"];
-// $address = $_GET["address"];
-// $phone = $_GET["phone"];
-// $cam2 = $_GET["item_0"];
-// $cam4 = $_GET["item_1"];
-// $cam5 = $_GET["item_2"];
-// $cam6 = $_GET["item_3"];
-// $cardType = $_GET["card"];
-// $cardNumber = $_GET["credit_card"];
-// $cardLast4 = substr($cardNumber, 15, 19);
-// $cardExpiration = $_GET["exp_date"];
-?>
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>Wind River Range Equipment</title>
-    <link rel="stylesheet" type="text/css" href="/week3/main.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<title>Wind River Range Equipment</title>
+	<link rel="stylesheet" type="text/css" href="/week3/main.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
-<?php $root = $_SERVER['DOCUMENT_ROOT']; ?>
-<header class="display-4">
-    <?php require $root . '/week3/header.php'; ?>
-</header>
-<nav>
-      <?php require $root . '/week3/nav.php'; ?>
-    </nav>
 
+<body>
+	<?php $root = $_SERVER['DOCUMENT_ROOT']; ?>
+	<header class="display-4">
+		<?php require $root . '/week3/header.php'; ?>
+	</header>
+	<nav>
+		<?php require $root . '/week3/nav.php'; ?>
+	</nav>
+	<main>
+		<?php
+		foreach ($_POST['products'] as $product) {
+			echo $product;
+		}
+		?>
+	</main>
+	<footer>
+		<?php require $root . '/week3/footer.php'; ?>
+	</footer>
+</body>
 
-<footer>
-    <?php require $root . '/week3/footer.php'; ?>
-</footer>
 </html>
