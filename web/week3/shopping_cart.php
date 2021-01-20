da61755934464aee0ebe25883595cd934729417e
mountain <Fiveoat@gmail.com>
2021-01-20 00:08:25 -0700

    x

--- web/week3/shopping_cart.php
+++ web/week3/shopping_cart.php
@@ -1,3 +1,45 @@
+<?php
+session_start();
+$_SESSION['cart'] = array();
+$root = $_SERVER['DOCUMENT_ROOT'];
+if (!isset($_SESSION["total"])) {
+    $_SESSION["total"] = 0;
+    for ($i = 0; $i < count($products); $i++) {
+        $_SESSION["qty"][$i] = 0;
+        $_SESSION["amounts"][$i] = 0;
+    }
+}
+if (isset($_GET['reset'])) {
+    if ($_GET["reset"] == 'true') {
+        unset($_SESSION["qty"]);
+        unset($_SESSION["amounts"]);
+        unset($_SESSION["total"]);
+        unset($_SESSION["cart"]);
+    }
+}
+if (isset($_GET["add"])) {
+    $i = $_GET["add"];
+    $qty = $_SESSION["qty"][$i] + 1;
+    $_SESSION["amounts"][$i] = $amounts[$i] * $qty;
+    $_SESSION["cart"][$i] = $i;
+    $_SESSION["qty"][$i] = $qty;
+}
+if (isset($_GET["delete"])) {
+    $i = $_GET["delete"];
+    $qty = $_SESSION["qty"][$i];
+    $qty--;
+    $_SESSION["qty"][$i] = $qty;
+    if ($qty == 0) {
+        $_SESSION["amounts"][$i] = 0;
+        unset($_SESSION["cart"][$i]);
+    } else {
+        $_SESSION["amounts"][$i] = $amounts[$i] * $qty;
+    }
+}
+$products = array("Size 6", "Size 5", "Size 4", "Size 3", "Size 2", "Size 1", "Size .75", "Size .5", "Size .4");
+$images = array("S6", "S5", "S4", "S3", "S2", "S1", "S075", "S05", "S04");
+$amounts = array(120, 110, 100, 90, 80, 70, 60, 50, 40);
+?>
 <!DOCTYPE html>
 <html>
 
