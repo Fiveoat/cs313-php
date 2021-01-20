fc5eb14ec8d093d07a1af03b1750930f9d26da3f
mountain <Fiveoat@gmail.com>
2021-01-20 00:05:33 -0700

    x

--- web/week3/store.php
+++ web/week3/store.php
@@ -38,7 +38,7 @@ if (isset($_GET["delete"])) {
 }
 $products = array("Size 6", "Size 5", "Size 4", "Size 3", "Size 2", "Size 1", "Size .75", "Size .5", "Size .4");
 $images = array("S6", "S5", "S4", "S3", "S2", "S1", "S075", "S05", "S04");
-$prices = array(120, 110, 100, 90, 80, 70, 60, 50, 40);
+$amounts = array(120, 110, 100, 90, 80, 70, 60, 50, 40);
 ?>
 <!DOCTYPE html>
 <html>
@@ -66,7 +66,7 @@ $prices = array(120, 110, 100, 90, 80, 70, 60, 50, 40);
                 for ($i = 0; $i < 3; $i++) {
                 ?>
                     <div class="col-md">
-                        <?php echo "<label><img src='/week3/images/{$images[$i]}.jpg' alt='black diamond camalot'>{$products[$i]} Price :${$prices[$i]} </label>"; ?>
+                        <?php echo "<label><img src='/week3/images/{$images[$i]}.jpg' alt='black diamond camalot'>{$products[$i]} Price :${$amounts[$i]} </label>"; ?>
                         <a href='?add=<?php echo ($i); ?>'>Add To Cart</a>
                     </div>
                 <?php }
@@ -77,7 +77,7 @@ $prices = array(120, 110, 100, 90, 80, 70, 60, 50, 40);
                 for ($i = 3; $i < 6; $i++) {
                 ?>
                     <div class="col-md">
-                        <?php echo "<label><img src='/week3/images/{$images[$i]}.jpg' alt='black diamond camalot'>{$products[$i]} Price :${$prices[$i]}</label>"; ?>
+                        <?php echo "<label><img src='/week3/images/{$images[$i]}.jpg' alt='black diamond camalot'>{$products[$i]} Price :${$amounts[$i]}</label>"; ?>
                         <a href='?add=<?php echo ($i); ?>'>Add To Cart</a>
                     </div>
                 <?php }
@@ -88,7 +88,7 @@ $prices = array(120, 110, 100, 90, 80, 70, 60, 50, 40);
                 for ($i = 6; $i < 9; $i++) {
                 ?>
                     <div class="col-md">
-                        <?php echo "<label><img src='/week3/images/{$images[$i]}.jpg' alt='black diamond camalot'>{$products[$i]} Price :${$prices[$i]}</label>"; ?>
+                        <?php echo "<label><img src='/week3/images/{$images[$i]}.jpg' alt='black diamond camalot'>{$products[$i]} Price :${$amounts[$i]}</label>"; ?>
                         <a href='?add=<?php echo ($i); ?>'>Add To Cart</a>
                     </div>
                 <?php }
@@ -96,46 +96,6 @@ $prices = array(120, 110, 100, 90, 80, 70, 60, 50, 40);
             </div>
         </div>
     </div>
-    <?php
-    if (isset($_SESSION["cart"])) {
-    ?>
-        <br /><br /><br />
-        <h2>Cart</h2>
-        <table>
-            <tr>
-                <th>Product</th>
-                <th width="10px">&nbsp;</th>
-                <th>Qty</th>
-                <th width="10px">&nbsp;</th>
-                <th>Amount</th>
-                <th width="10px">&nbsp;</th>
-                <th>Action</th>
-            </tr>
-            <?php
-            $total = 0;
-            foreach ($_SESSION["cart"] as $i) {
-            ?>
-                <tr>
-                    <td><?php echo ($products[$_SESSION["cart"][$i]]); ?></td>
-                    <td width="10px">&nbsp;</td>
-                    <td><?php echo ($_SESSION["qty"][$i]); ?></td>
-                    <td width="10px">&nbsp;</td>
-                    <td><?php echo ($_SESSION["amounts"][$i]); ?></td>
-                    <td width="10px">&nbsp;</td>
-                    <td><a href="?delete=<?php echo ($i); ?>">Delete from cart</a></td>
-                </tr>
-            <?php
-                $total = $total + $_SESSION["amounts"][$i];
-            }
-            $_SESSION["total"] = $total;
-            ?>
-            <tr>
-                <td colspan="7">Total : <?php echo ($total); ?></td>
-            </tr>
-        </table>
-    <?php
-    }
-    ?>
     <!-- <form method="POST" action="shopping_cart.php">
             <div class="container">
                 <div class="row">
