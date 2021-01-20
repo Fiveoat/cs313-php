1a170c3db2be30bfce235d002670a24583c1940a
mountain <Fiveoat@gmail.com>
2021-01-19 23:46:45 -0700

    x

--- web/week3/store.php
+++ web/week3/store.php
@@ -36,9 +36,9 @@ if (isset($_GET["delete"])) {
         $_SESSION["amounts"][$i] = $amounts[$i] * $qty;
     }
 }
-$products = array("Size 6", "Size 5", "Size 4");
-$images = array("");
-$prices = array("120", "110", "100");
+$products = array("Size 6", "Size 5", "Size 4", "Size 3", "Size 2", "Size 1", "Size .75", "Size .5", "Size .4");
+$images = array("S6", "S5", "S4", "S3", "S2", "S1", "S075", "S05", "S04");
+$prices = array("120", "110", "100", "90", "80", "70", "60", "50", "40");
 ?>
 <!DOCTYPE html>
 <html>
