d55c99f9512c87e1b626682d392e93c57d68938b
mountain <Fiveoat@gmail.com>
2021-01-19 20:45:42 -0700

    x

--- web/week3/store.php
+++ web/week3/store.php
@@ -1,85 +1,50 @@
 <?php
-   session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
-   <head>
-      <title>Wind River Range Equipment</title>
-	  <link rel="stylesheet" type="text/css" href="week3/main.css">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-   </head>
-   <body>
-	  <br />
-	  <h1 class="display-1">Wind River Range Equipment</h1>
-	
-      <div id="cams">
-	     <h2>Black Diamond Camalots</h2>
 
-		 <div class="product" class="container-md">
-		    <a href="">
-			   <img id="6" src="/week3/images/S6.jpg" alt="black diamond camalot">
-			   <span class="cam">Black Diamond Camalot Size: 6</span>
-			</a>
-         </div>
-         <div class="product" class="container-md">
-		    <a href="">
-			   <img id="5" src="/week3/images/S5.jpg" alt="black diamond camalot">
-			   <span class="cam">Black Diamond Camalot Size: 5</span>
-			</a>
-         </div>
-         <div class="product" class="container-md">
-		    <a href="">
-			   <img id="4" src="/week3/images/S4.jpg" alt="black diamond camalot">
-			   <span class="cam">Black Diamond Camalot Size: 4</span>
-			</a>
-         </div>
-         <div class="product" class="container-md">
-		    <a href="">
-			   <img id="3" src="/week3/images/S3.jpg" alt="black diamond camalot">
-			   <span class="cam">Black Diamond Camalot Size: 3</span>
-			</a>
-         </div>
-         <div class="product" class="container-md">
-		    <a href="">
-			   <img id="2" src="/week3/images/S2.jpg" alt="black diamond camalot">
-			   <span class="cam">Black Diamond Camalot Size: 2</span>
-			</a>
-         </div>
-         <div class="product" class="container-md">
-		    <a href="">
-			   <img id="1" src="/week3/images/S1.jpg" alt="black diamond camalot">
-			   <span class="cam">Black Diamond Camalot Size: 1</span>
-			</a>
-         </div>
-         <div class="product" class="container-md">
-		    <a href="">
-			   <img id="075" src="/week3/images/S075.jpg" alt="black diamond camalot">
-			   <span class="cam">Black Diamond Camalot Size: .75</span>
-			</a>
-         </div>
-         <div class="product" class="container-md">
-		    <a href="">
-			   <img id="05" src="/week3/images/S05.jpg" alt="black diamond camalot">
-			   <span class="cam">Black Diamond Camalot Size: .5</span>
-			</a>
-         </div>
-		 <div class="product" class="container-md">
-		    <a href="04">
-			   <img id="" src="/week3/images/S04.jpg" alt="black diamond camalot">
-			   <span class="cam">Black Diamond Camalot Size: .4</span>
-			</a>
-         </div>
+<head>
+    <title>Wind River Range Equipment</title>
+    <link rel="stylesheet" type="text/css" href="week3/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
 
-            <input type="checkbox" name="products[]" id="S6" value="S6"><label for="S6"> <img id="" src="/week3/images/S04.jpg" alt="black diamond camalot"> Black Diamond Camalot Size 6</label><br/>
-			<input type="checkbox" name="products[]" id="S5" value="S5"><label for="S5">Black Diamond Camalot Size 5</label><br/>
-			<input type="checkbox" name="products[]" id="S4" value="S4"><label for="S4">Black Diamond Camalot Size 4</label><br/>
-			<input type="checkbox" name="products[]" id="S3" value="S3"><label for="S3">Black Diamond Camalot Size 3</label><br/>
-			<input type="checkbox" name="products[]" id="S2" value="S2"><label for="S2">Black Diamond Camalot Size 2</label><br/>
-			<input type="checkbox" name="products[]" id="S1" value="S1"><label for="S1">Black Diamond Camalot Size 1</label><br/>
-            <input type="checkbox" name="products[]" id="S075" value="S075"><label for="S075">Black Diamond Camalot Size .75</label><br/>
-            <input type="checkbox" name="products[]" id="S05" value="S05"><label for="S05">Black Diamond Camalot Size .5</label><br/>
-            <input type="checkbox" name="products[]" id="S04" value="S04"><label for="S04">Black Diamond Camalot Size .4</label><br/>
+<body>
+    <br />
+    <h1 class="display-1">Wind River Range Equipment</h1>
 
-	  </div>
-   </body>
+    <div id="cams">
+        <h2>Black Diamond Camalots</h2>
+
+        <div class="container-md">
+            <input type="checkbox" name="products[]" id="S6" value="S6"><label for="S6"><img id="" src="/week3/images/S6.jpg" alt="black diamond camalot"> Black Diamond Camalot Size 6</label><br />
+        </div>
+        <div class="container-md">
+            <input type="checkbox" name="products[]" id="S5" value="S5"><label for="S5"><img id="" src="/week3/images/S5.jpg" alt="black diamond camalot"> Black Diamond Camalot Size 5</label><br />
+        </div>
+        <div class="container-md">
+            <input type="checkbox" name="products[]" id="S4" value="S4"><label for="S4"><img id="" src="/week3/images/S4.jpg" alt="black diamond camalot"> Black Diamond Camalot Size 4</label><br />
+        </div>
+        <div class="container-md">
+            <input type="checkbox" name="products[]" id="S3" value="S3"><label for="S3"><img id="" src="/week3/images/S3.jpg" alt="black diamond camalot"> Black Diamond Camalot Size 3</label><br />
+        </div>
+        <div class="container-md">
+            <input type="checkbox" name="products[]" id="S2" value="S2"><label for="S2"><img id="" src="/week3/images/S2.jpg" alt="black diamond camalot"> Black Diamond Camalot Size 2</label><br />
+        </div>
+        <div class="container-md">
+            <input type="checkbox" name="products[]" id="S1" value="S1"><label for="S1"><img id="" src="/week3/images/S1.jpg" alt="black diamond camalot"> Black Diamond Camalot Size 1</label><br />
+        </div>
+        <div class="container-md">
+            <input type="checkbox" name="products[]" id="S075" value="S075"><label for="S075"><img id="" src="/week3/images/S075.jpg" alt="black diamond camalot"> Black Diamond Camalot Size .75</label><br />
+        </div>
+        <div class="container-md">
+            <input type="checkbox" name="products[]" id="S05" value="S05"><label for="S05"><img id="" src="/week3/images/S05.jpg" alt="black diamond camalot"> Black Diamond Camalot Size .5</label><br />
+        </div>
+        <div class="container-md">
+            <input type="checkbox" name="products[]" id="S04" value="S04"><label for="S04"><img id="" src="/week3/images/S04.jpg" alt="black diamond camalot"> Black Diamond Camalot Size .4</label><br />
+        </div>
+
+</body>
+
 </html>
