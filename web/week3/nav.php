ffe031efb8d2ebf71558292d4b379a4a85537d5a
mountain <Fiveoat@gmail.com>
2021-01-19 21:19:20 -0700

    x

--- web/week3/nav.php
+++ web/week3/nav.php
@@ -6,6 +6,6 @@
         <a class="nav-link active" aria-current="page" href="/week3/checkout.php">Checkout&ensp;</a>
     </li>
     <li class="nav-item">
-        <a href="shopping_cart.php" id="shoppingCart"> <img class="nav-cart" id="shoppingCartImage" src="images/cart.png" alt="Shopping Cart"></a>';
+        <a href="shopping_cart.php" id="shoppingCart"> Cart<img class="nav-cart" id="shoppingCartImage" src="images/cart.png" alt="Shopping Cart"></a>
     </li>
 </ul>
