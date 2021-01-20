ad162ed687714eb148148a453aa121ab39f81c6f
mountain <Fiveoat@gmail.com>
2021-01-19 20:25:14 -0700

    x

---
+++ web/week3/store.php
@@ -0,0 +1,75 @@
+<?php
+   session_start();
+?>
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Wind River Range Equipment</title>
+	  <!-- <link rel="stylesheet" type="text/css" href="week03.css"> -->
+	  <!-- <link rel="stylesheet" type="text/css" href="menu.css"> -->
+   </head>
+   <body>
+	  <br />
+	  <h1>Wind River Range Equipment</h1>
+	
+      <div id="cams">
+	     <h2>Black Diamond Camalots</h2>
+
+		 <div class="product">
+		    <a href="">
+			   <img id="6" src="/week3/images/S6.jpg" alt="black diamond camalot">
+			   <span class="cam">Black Diamond Camalot Size: 6</span>
+			</a>
+         </div>
+         <div class="product">
+		    <a href="">
+			   <img id="5" src="/week3/images/S5.jpg" alt="black diamond camalot">
+			   <span class="cam">Black Diamond Camalot Size: 5</span>
+			</a>
+         </div>
+         <div class="product">
+		    <a href="">
+			   <img id="4" src="/week3/images/S4.jpg" alt="black diamond camalot">
+			   <span class="cam">Black Diamond Camalot Size: 4</span>
+			</a>
+         </div>
+         <div class="product">
+		    <a href="">
+			   <img id="3" src="/week3/images/S3.jpg" alt="black diamond camalot">
+			   <span class="cam">Black Diamond Camalot Size: 3</span>
+			</a>
+         </div>
+         <div class="product">
+		    <a href="">
+			   <img id="2" src="/week3/images/S2.jpg" alt="black diamond camalot">
+			   <span class="cam">Black Diamond Camalot Size: 2</span>
+			</a>
+         </div>
+         <div class="product">
+		    <a href="">
+			   <img id="1" src="/week3/images/S1.jpg" alt="black diamond camalot">
+			   <span class="cam">Black Diamond Camalot Size: 1</span>
+			</a>
+         </div>
+         <div class="product">
+		    <a href="">
+			   <img id="075" src="/week3/images/S075.jpg" alt="black diamond camalot">
+			   <span class="cam">Black Diamond Camalot Size: .75</span>
+			</a>
+         </div>
+         <div class="product">
+		    <a href="">
+			   <img id="05" src="/week3/images/S05.jpg" alt="black diamond camalot">
+			   <span class="cam">Black Diamond Camalot Size: .5</span>
+			</a>
+         </div>
+		 <div class="product">
+		    <a href="04">
+			   <img id="" src="/week3/images/S04.jpg" alt="black diamond camalot">
+			   <span class="cam">Black Diamond Camalot Size: .4</span>
+			</a>
+         </div>
+
+	  </div>
+   </body>
+</html>
