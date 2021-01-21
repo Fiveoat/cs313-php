f1b64f46144542f5dbbac8c431a1b25b3ed2f5e2
mountain <Fiveoat@gmail.com>
2021-01-21 01:30:43 -0700

    x

--- web/week3/nav.php
+++ web/week3/nav.php
@@ -6,9 +6,9 @@ session_start();
         <a class="nav-link" href="/week3/store.php">Store&ensp;</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link active" href="/week3/shopping_cart.php"> Cart&ensp;<img class="nav-cart" id="shoppingCartImage" src="images/cart.png" alt="Shopping Cart"><?php echo " ".count($_SESSION['cart'])?></a>
+        <a class="nav-link active" aria-current="page" href="/week3/checkout.php">Checkout&ensp;</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="/week3/checkout.php">Checkout&ensp;</a>
+        <a class="nav-link active" href="/week3/shopping_cart.php"> Cart&ensp;<img class="nav-cart" id="shoppingCartImage" src="images/cart.png" alt="Shopping Cart"><?php echo " ".count($_SESSION['cart'])?></a>
     </li>
 </ul>
