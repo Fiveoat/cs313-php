c5cd260b41f8e6f9a3e1513a1161ba405bd391f7
mountain <Fiveoat@gmail.com>
2021-01-21 00:42:27 -0700

    x

--- web/week3/remove_from_cart.php
+++ web/week3/remove_from_cart.php
@@ -1,10 +1,5 @@
 <?php
 session_start();
-
-foreach ($_SESSION['cart'] as $id){
-    if($id == $_GET['id']){
-      unset($_SESSION['cart'][$_GET['id']]);
-    }
-  }
+unset($_SESSION['cart'][$_GET['id']]);
 header("location: shopping_cart.php");
 ?>
