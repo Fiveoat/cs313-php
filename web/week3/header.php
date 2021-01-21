8a2fbcb2c275ff694ab80d3d5c12ad12fed50417
mountain <Fiveoat@gmail.com>
2021-01-20 22:03:36 -0700

    x

--- web/week3/header.php
+++ web/week3/header.php
@@ -1,1 +1,4 @@
-<h1 class="display-1 justify-content-end">Wind River Range Equipment&ensp;</h1>
+<div class="jumbotron" class="display-4">
+    <h1 class="d-flex justify-content-center">Wind River Range Equipment</h1>
+    <h4 class="d-flex justify-content-center">Since 1957</h4>
+</div>
