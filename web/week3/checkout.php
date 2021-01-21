6b2b13cfbd33a8f347cda9a25d864ff3f227e33c
mountain <Fiveoat@gmail.com>
2021-01-21 01:05:49 -0700

    x

--- web/week3/checkout.php
+++ web/week3/checkout.php
@@ -24,7 +24,7 @@ session_start();
             foreach ($_SESSION['cart'] as $id) {
                 echo "<div class='col-sm'>
                         <img src='/week3/images/S{$id}.jpg' alt='black diamond camalot'>Size {$id}
-                        <a href='/week3/remove_from_cart.php?id={$id}'>Delte</a>
+                        <a href='/week3/remove_from_cart.php?id={$id}'>Delete</a>
                      </div><br>";
             }
             ?>
