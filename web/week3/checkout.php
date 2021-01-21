74d78d559e8d65b940a27d8a0f01dc18b4ac3881
mountain <Fiveoat@gmail.com>
2021-01-21 00:46:32 -0700

    x

--- web/week3/checkout.php
+++ web/week3/checkout.php
@@ -1,47 +1,6 @@
 <?php
 session_start();
-echo $_SESSION["cart"][0];
-if (isset($_SESSION["cart"])) {
-?>
-    <br /><br /><br />
-    <h2>Cart</h2>
-    <table>
-        <tr>
-            <th>Product</th>
-            <th width="10px">&nbsp;</th>
-            <th>Qty</th>
-            <th width="10px">&nbsp;</th>
-            <th>Amount</th>
-            <th width="10px">&nbsp;</th>
-            <th>Action</th>
-        </tr>
-        <?php
-        $total = 0;
-        foreach ($_SESSION["cart"] as $i) {
-        ?>
-            <tr>
-                <td><?php echo ($products[$_SESSION["cart"][$i]]); ?></td>
-                <td width="10px">&nbsp;</td>
-                <td><?php echo ($_SESSION["qty"][$i]); ?></td>
-                <td width="10px">&nbsp;</td>
-                <td><?php echo ($_SESSION["amounts"][$i]); ?></td>
-                <td width="10px">&nbsp;</td>
-                <td><a href="?delete=<?php echo ($i); ?>">Delete from cart</a></td>
-            </tr>
-        <?php
-            $total = $total + $_SESSION["amounts"][$i];
-        }
-        $_SESSION["total"] = $total;
-        ?>
-        <tr>
-            <td colspan="7">Total : <?php echo ($total); ?></td>
-        </tr>
-    </table>
-    <form action="checkout.php" method="POST">
-        <input type="submit" name="Go To Cart">
-    </form>
-<?php
-}
+echo $_SESSION['cart'][0];
 ?>
 <!DOCTYPE html>
 <html>
--- web/week3/remove_from_cart.php
+++ web/week3/remove_from_cart.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
+echo $_GET['id'];
 if (($key = array_search($_GET['id'], $array)) !== false) {
     unset($array[$key]);
 }
-header("location: shopping_cart.php");
+// header("location: shopping_cart.php");
 ?>
