860edc3961f503765ec03bfd771626e30ad7d717
mountain <Fiveoat@gmail.com>
2021-01-21 01:11:41 -0700

    x

--- web/week3/checkout.php
+++ web/week3/checkout.php
@@ -57,26 +57,6 @@ session_start();
         <label for="zip">Zip</label>
         <input type="text" placeholder="Zip" id="zip" name="zip">
         <br />
-
-        :<br />
-        <input type="radio" name="major" value="Computer Science" id="major-cs"><label for="major-cs">Computer Science</label><br />
-        <input type="radio" name="major" value="Web Design and Development" id="major-wdd"><label for="major-wdd">Web Design and Development</label><br />
-        <input type="radio" name="major" value="Computer Information Technology" id="major-cit"><label for="major-cit">Computer Information Technology</label><br />
-        <input type="radio" name="major" value="Computer Engineering" id="major-ce"><label for="major-ce">Computer Engineering</label><br />
-        <br />
-
-        <input type="checkbox" name="places[]" id="place-na" value="North America"><label for="place-na">North America</label><br />
-        <input type="checkbox" name="places[]" id="place-sa" value="South America"><label for="place-sa">South America</label><br />
-        <input type="checkbox" name="places[]" id="place-asia" value="Asia"><label for="place-asia">Asia America</label><br />
-        <input type="checkbox" name="places[]" id="place-eu" value="Europe"><label for="place-eu">Europe</label><br />
-        <input type="checkbox" name="places[]" id="place-af" value="Africa"><label for="place-af">Africa</label><br />
-        <input type="checkbox" name="places[]" id="place-aus" value="Australia"><label for="place-aus">Australia</label><br />
-        <input type="checkbox" name="places[]" id="place-ant" value="Antarctica"><label for="place-ant">Antarctica</label><br />
-        <br />
-
-        <label for="comments">Comments:</label><br />
-        <textarea id="comments" name="comments" rows="4" cols="50"></textarea>
-        <br />
         <input type="submit" value="Submit">
     </form>
 
