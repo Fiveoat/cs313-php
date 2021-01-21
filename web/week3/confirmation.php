024756e956515bf3f0963b3912d2886f2542f221
mountain <Fiveoat@gmail.com>
2021-01-21 01:23:23 -0700

    x

--- web/week3/confirmation.php
+++ web/week3/confirmation.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $name = htmlspecialchars($_POST["name"]);
 $email = htmlspecialchars($_POST["email"]);
 $state = htmlspecialchars($_POST["state"]);
@@ -10,10 +11,19 @@ $street = htmlspecialchars($_POST["street"]);
 <html>
 
 <head>
-    <title>Order Confirmation</title>
+    <title>Wind River Range Equipment</title>
+    <link rel="stylesheet" type="text/css" href="/week3/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 
 <body>
+    <?php $root = $_SERVER['DOCUMENT_ROOT']; ?>
+    <header class="display-4">
+        <?php require $root . '/week3/header.php'; ?>
+    </header>
+    <nav>
+        <?php require $root . '/week3/nav.php'; ?>
+    </nav>
     <h1>Order Confirmation</h1>
     <?php
     echo "<h1>Thank you, {$name} for your order.</h1>
@@ -25,6 +35,9 @@ $street = htmlspecialchars($_POST["street"]);
     <h6>Zip: {$zip}</h6>
     </h6>"
     ?>
+    <footer>
+        <?php require $root . '/week3/footer.php'; ?>
+    </footer>
 </body>
 
 
