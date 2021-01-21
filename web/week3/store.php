6026af3a14bafd35f8500b3c03ef4dffcf602817
mountain <Fiveoat@gmail.com>
2021-01-20 22:10:28 -0700

    x

--- web/week3/store.php
+++ web/week3/store.php
@@ -20,51 +20,57 @@ $root = $_SERVER['DOCUMENT_ROOT'];
         <?php require $root . '/week3/nav.php'; ?>
     </nav>
     <br>
-    <form method="POST" action="shopping_cart.php">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md">
-                        <input class="form-check-input" type="checkbox" name="products[]" id="S6" value="S6"><label for="S6"><img id="" src="/week3/images/S6.jpg" alt="black diamond camalot"> Size 6 Price: $130 </label><br />
-                    </div>
-                    <div class="col-md">
-                        <input class="form-check-input" type="checkbox" name="products[]" id="S5" value="S5"><label for="S5"><img id="" src="/week3/images/S5.jpg" alt="black diamond camalot"> Size 5 Price: $120 </label><br />
-                    </div>
-                    <div class="col-md">
-                        <input class="form-check-input" type="checkbox" name="products[]" id="S4" value="S4"><label for="S4"><img id="" src="/week3/images/S4.jpg" alt="black diamond camalot"> Size 4 Price: $110 </label><br />
-                    </div>
+
+    <div class="container">
+        <div class="row">
+            <form method="POST" action="shopping_cart.php">
+                <div class="col-md">
+                    <input class="form-check-input" type="checkbox" name="products[]" id="S6" value="S6"><label for="S6"><img id="" src="/week3/images/S6.jpg" alt="black diamond camalot"> Size 6 Price: $130 </label><br />
                 </div>
-            </div>
-            <br><br><br><br>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md">
-                        <input class="form-check-input" type="checkbox" name="products[]" id="S3" value="S3"><label for="S3"><img id="" src="/week3/images/S3.jpg" alt="black diamond camalot"> Size 3 Price: $100 </label><br />
-                    </div>
-                    <div class="col-md">
-                        <input class="form-check-input" type="checkbox" name="products[]" id="S2" value="S2"><label for="S2"><img id="" src="/week3/images/S2.jpg" alt="black diamond camalot"> Size 2 Price: $90 </label><br />
-                    </div>
-                    <div class="col-md">
-                        <input class="form-check-input" type="checkbox" name="products[]" id="S1" value="S1"><label for="S1"><img id="" src="/week3/images/S1.jpg" alt="black diamond camalot"> Size 1 Price: $80 </label><br />
-                    </div>
+            </form>
+            <form method="POST" action="shopping_cart.php">
+                <div class="col-md">
+                    <input class="form-check-input" type="checkbox" name="products[]" id="S5" value="S5"><label for="S5"><img id="" src="/week3/images/S5.jpg" alt="black diamond camalot"> Size 5 Price: $120 </label><br />
                 </div>
-            </div>
-            <br><br><br><br>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md">
-                        <input class="form-check-input" type="checkbox" name="products[]" id="S075" value="S075"><label for="S075"><img id="" src="/week3/images/S075.jpg" alt="black diamond camalot"> Size .75 Price: $70 </label><br />
-                    </div>
-                    <div class="col-md">
-                        <input class="form-check-input" type="checkbox" name="products[]" id="S05" value="S05"><label for="S05"><img id="" src="/week3/images/S05.jpg" alt="black diamond camalot"> Size .5 Price: $60 </label><br />
-                    </div>
-                    <div class="col-md">
-                        <input class="form-check-input" type="checkbox" name="products[]" id="S04" value="S04"><label for="S04"><img id="" src="/week3/images/S04.jpg" alt="black diamond camalot"> Size .4 Price: $50 </label><br />
-                    </div>
+            </form>
+            <form method="POST" action="shopping_cart.php">
+                <div class="col-md">
+                    <input class="form-check-input" type="checkbox" name="products[]" id="S4" value="S4"><label for="S4"><img id="" src="/week3/images/S4.jpg" alt="black diamond camalot"> Size 4 Price: $110 </label><br />
                 </div>
+            </form>
+        </div>
+    </div>
+    <br><br><br><br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md">
+                <input class="form-check-input" type="checkbox" name="products[]" id="S3" value="S3"><label for="S3"><img id="" src="/week3/images/S3.jpg" alt="black diamond camalot"> Size 3 Price: $100 </label><br />
             </div>
-            <br><br><br>
-            <input type="submit" class="btn btn-outline-secondary" value="Add To Cart">
-        </form>
+            <div class="col-md">
+                <input class="form-check-input" type="checkbox" name="products[]" id="S2" value="S2"><label for="S2"><img id="" src="/week3/images/S2.jpg" alt="black diamond camalot"> Size 2 Price: $90 </label><br />
+            </div>
+            <div class="col-md">
+                <input class="form-check-input" type="checkbox" name="products[]" id="S1" value="S1"><label for="S1"><img id="" src="/week3/images/S1.jpg" alt="black diamond camalot"> Size 1 Price: $80 </label><br />
+            </div>
+        </div>
+    </div>
+    <br><br><br><br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md">
+                <input class="form-check-input" type="checkbox" name="products[]" id="S075" value="S075"><label for="S075"><img id="" src="/week3/images/S075.jpg" alt="black diamond camalot"> Size .75 Price: $70 </label><br />
+            </div>
+            <div class="col-md">
+                <input class="form-check-input" type="checkbox" name="products[]" id="S05" value="S05"><label for="S05"><img id="" src="/week3/images/S05.jpg" alt="black diamond camalot"> Size .5 Price: $60 </label><br />
+            </div>
+            <div class="col-md">
+                <input class="form-check-input" type="checkbox" name="products[]" id="S04" value="S04"><label for="S04"><img id="" src="/week3/images/S04.jpg" alt="black diamond camalot"> Size .4 Price: $50 </label><br />
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <input type="submit" class="btn btn-outline-secondary" value="Add To Cart">
+    </form>
     <footer>
         <?php require $root . '/week3/footer.php'; ?>
     </footer>
