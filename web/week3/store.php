bbfd5bb8a4b0cf6a60d213058d488b92f3c5c82b
mountain <Fiveoat@gmail.com>
2021-01-20 22:23:41 -0700

    x

--- web/week3/store.php
+++ web/week3/store.php
@@ -24,13 +24,16 @@ $root = $_SERVER['DOCUMENT_ROOT'];
     <div class="container">
         <div class="row">
             <form method="POST" action="shopping_cart.php" class="col-md">
-                <input class="btn btn-primary" type="submit" name="products[]" id="S6" value="Add To Cart: Size 6"><label for="S6"><img src="/week3/images/S6.jpg" alt="black diamond camalot"></label><br />
+                <label for="S6"><img src="/week3/images/S6.jpg" alt="black diamond camalot"></label>
+                <input class="btn btn-primary" type="submit" name="products[]" id="S6" value="Add To Cart"><br />
             </form>
             <form method="POST" action="shopping_cart.php" class="col-md">
-                <input class="btn btn-primary" type="submit" name="products[]" id="S5" value="Add To Cart: Size 5"><label for="S5"><img src="/week3/images/S5.jpg" alt="black diamond camalot"></label><br />
+                <label for="S5"><img src="/week3/images/S5.jpg" alt="black diamond camalot"></label>
+                <input class="btn btn-primary" type="submit" name="products[]" id="S5" value="Add To Cart"><br />
             </form>
             <form method="POST" action="shopping_cart.php" class="col-md">
-                <input class="btn btn-primary" type="submit" name="products[]" id="S4" value="Add To Cart: Size 4"><label for="S4"><img src="/week3/images/S4.jpg" alt="black diamond camalot"></label><br />
+                <label for="S4"><img src="/week3/images/S4.jpg" alt="black diamond camalot"></label>
+                <input class="btn btn-primary" type="submit" name="products[]" id="S4" value="Add To Cart"><br />
             </form>
         </div>
     </div>
