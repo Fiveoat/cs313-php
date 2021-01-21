59ace62c65f79c9927c5cedf5b5f524aa04fb8fe
mountain <Fiveoat@gmail.com>
2021-01-21 00:48:07 -0700

    x

--- web/week3/remove_from_cart.php
+++ web/week3/remove_from_cart.php
@@ -2,7 +2,11 @@
 session_start();
 echo $_GET['id'];
 if (($key = array_search($_GET['id'], $array)) !== false) {
+    echo "REOMVED";
     unset($array[$key]);
+} else {
+    echo "KPET";
 }
+
 // header("location: shopping_cart.php");
 ?>
