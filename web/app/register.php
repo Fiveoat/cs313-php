386fc8c48ffd73c97431886a50c0f4fbac909acd
mountain <Fiveoat@gmail.com>
2021-01-31 23:36:06 -0700

    x

--- web/app/register.php
+++ web/app/register.php
@@ -19,16 +19,16 @@ $root = $_SERVER['DOCUMENT_ROOT'];
         <?php require $root . '/app/nav.php'; ?>
     </nav>
     <main>
-        <h1 class="display-4">Welcome. Sign Up!</h1>
+        <h1 class="display-4">Welcome! Sign Up!</h1>
         <form>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
