01e34e0a9b7659284081d34a7082244f2ec260c1
mountain <Fiveoat@gmail.com>
2021-01-31 23:43:39 -0700

    x

--- web/app/sign_in.php
+++ web/app/sign_in.php
@@ -7,7 +7,7 @@ $root = $_SERVER['DOCUMENT_ROOT'];
 
 <head>
     <title>Houselr</title>
-    <link rel="stylesheet" type="text/css" href="/week3/main.css">
+    <link rel="stylesheet" type="text/css" href="/app/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 
