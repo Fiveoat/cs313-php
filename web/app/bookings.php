14c0c2afb2ab81244935871fa5019dba66da34b4
mountain <Fiveoat@gmail.com>
2021-01-31 23:44:37 -0700

    x

---
+++ web/app/bookings.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+$root = $_SERVER['DOCUMENT_ROOT'];
+require $root . '/app/connection.php';
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Houselr</title>
+    <link rel="stylesheet" type="text/css" href="/app/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+
+<body>
+    <header class="display-4">
+        <?php require $root . '/app/header.php'; ?>
+    </header>
+    <nav>
+        <?php require $root . '/app/nav.php'; ?>
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+        <?php require $root . '/app/footer.php'; ?>
+    </footer>
+
+</body>
+
+</html>
