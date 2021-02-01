ce29ea57c680070e647284a88debdcfd270b1a97
mountain <Fiveoat@gmail.com>
2021-01-31 23:33:48 -0700

    x

--- web/app/register.php
+++ web/app/register.php
@@ -19,6 +19,7 @@ $root = $_SERVER['DOCUMENT_ROOT'];
         <?php require $root . '/app/nav.php'; ?>
     </nav>
     <main>
+        <h1 class="display-4">Welcome. Sign Up!</h1>
         <form>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
@@ -28,10 +29,6 @@ $root = $_SERVER['DOCUMENT_ROOT'];
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
--- web/app/sign_in.php
+++ web/app/sign_in.php
@@ -29,9 +29,10 @@ $root = $_SERVER['DOCUMENT_ROOT'];
                 <label for="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1">
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <label for="inputPassword5" class="form-label">Password</label>
+            <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+            <div id="passwordHelpBlock" class="form-text">
+                Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
