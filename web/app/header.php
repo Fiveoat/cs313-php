6dae470cc3255e2e7aeb0ca48bf4af52b3a7b003
mountain <Fiveoat@gmail.com>
2021-01-31 23:15:33 -0700

    x

--- web/app/header.php
+++ web/app/header.php
@@ -1,1 +1,1 @@
-<h1 class="display-1 justify-content-end">🌚 Wind River Range Equipment 🌝 &ensp;</h1>
+<h1 class="display-1 justify-content-end">🌚 Housler 🌝 &ensp;</h1>
--- web/app/nav.php
+++ web/app/nav.php
@@ -1,9 +1,15 @@
 <ul class="nav justify-content-end">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="/index.php">Home&ensp;</a>
+    <a class="nav-link active" aria-current="page" href="app/index.php">Home&ensp;</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="/assignments.php">Assignments&ensp;</a>
+    <a class="nav-link" href="app/sign_in.php">Assignments&ensp;</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="app/account.php">My Account&ensp;</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="app/bookings.php">Bookings&ensp;</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="#">Other&ensp;</a>
