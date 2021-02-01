30cf0895318f131014340992674e9f69997182a0
mountain <Fiveoat@gmail.com>
2021-01-31 23:16:30 -0700

    x

--- web/app/nav.php
+++ web/app/nav.php
@@ -3,15 +3,9 @@
     <a class="nav-link active" aria-current="page" href="app/index.php">Home&ensp;</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="app/sign_in.php">Assignments&ensp;</a>
-  </li>
-  <li class="nav-item">
     <a class="nav-link" href="app/account.php">My Account&ensp;</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="app/bookings.php">Bookings&ensp;</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Other&ensp;</a>
   </li>
 </ul>
