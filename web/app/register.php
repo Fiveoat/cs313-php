e422f6b2d459ebb30630f1d5c28dc8ac4d86806a
mountain <Fiveoat@gmail.com>
2021-02-02 23:13:11 -0700

    x

--- web/app/register.php
+++ web/app/register.php
@@ -20,44 +20,50 @@ $root = $_SERVER['DOCUMENT_ROOT'];
     </nav>
     <main>
         <h1 class="display-4">Welcome! Sign Up!</h1>
-        <form>
-            <div class="mb-3">
-                <label for="first-name" class="form-label">First Name</label>
-                <input type="first-name" class="form-control" id="first-name">
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                    <form>
+                        <div class="mb-3">
+                            <label for="first-name" class="form-label">First Name</label>
+                            <input type="first-name" class="form-control" id="first-name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="last-name" class="form-label">Last Name</label>
+                            <input type="last-name" class="form-control" id="last-name">
+                        </div>
+                        <label for="inputPassword5" class="form-label">Password</label>
+                        <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+                        <div id="passwordHelpBlock" class="form-text">
+                            Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email Address</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="city" class="form-label">City</label>
+                            <input type="city" class="form-control" id="city">
+                        </div>
+                        <div class="mb-3">
+                            <label for="country-code" class="form-label">Country Code</label>
+                            <input type="country-code" class="form-control" id="country-code">
+                        </div>
+                        <div>
+                            <label for="last-name" class="form-label">Home Size</label>
+                            <select class="form-select" aria-label="Default select example">
+                                <option value="1">Large > 2000 sq ft.</option>
+                                <option value="2">Medium < 2000 sq ft.</option>
+                                <option value="3">Small < 1000 sq ft.</option>
+                            </select>
+                        </div>
+                        <br><br>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="last-name" class="form-label">Last Name</label>
-                <input type="last-name" class="form-control" id="last-name">
-            </div>
-            <label for="inputPassword5" class="form-label">Password</label>
-            <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
-            <div id="passwordHelpBlock" class="form-text">
-                Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email Address</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-                <label for="city" class="form-label">City</label>
-                <input type="city" class="form-control" id="city">
-            </div>
-            <div class="mb-3">
-                <label for="country-code" class="form-label">Country Code</label>
-                <input type="country-code" class="form-control" id="country-code">
-            </div>
-            <div>
-                <label for="last-name" class="form-label">Home Size</label>
-                <select class="form-select" aria-label="Default select example">
-                    <option value="1">Large > 2000 sq ft.</option>
-                    <option value="2">Medium < 2000 sq ft.</option>
-                    <option value="3">Small < 1000 sq ft.</option>
-                </select>
-            </div>
-            <br><br>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        </div>
     </main>
     <footer>
         <?php require $root . '/app/footer.php'; ?>
