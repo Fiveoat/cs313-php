fb8a3bba7939993bd7f5a63759ffa6276363ee28
mountain <Fiveoat@gmail.com>
2021-02-02 22:20:41 -0700

    x

--- web/app/sign_in.php
+++ web/app/sign_in.php
@@ -28,7 +28,6 @@ $root = $_SERVER['DOCUMENT_ROOT'];
             <label for="inputPassword5" class="form-label">Password</label>
             <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
             <div id="passwordHelpBlock" class="form-text">
-                Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
             </div>
             <br>
             <button type="submit" class="btn btn-primary">Submit</button>
