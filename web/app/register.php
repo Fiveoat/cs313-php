e335d956b7a19aab0e0ee85a5217b4dcf23d8e5f
mountain <Fiveoat@gmail.com>
2021-02-02 22:22:49 -0700

    x

--- web/app/register.php
+++ web/app/register.php
@@ -29,9 +29,15 @@ $root = $_SERVER['DOCUMENT_ROOT'];
                 <label for="last-name" class="form-label">Last Name</label>
                 <input type="last-name" class="form-control" id="last-name">
             </div>
+            <label for="inputPassword5" class="form-label">Password</label>
+            <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+            <div id="passwordHelpBlock" class="form-text">
+                Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+            </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email Address</label>
                 <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="city" class="form-label">City</label>
--- web/app/sign_in.php
+++ web/app/sign_in.php
@@ -6,7 +6,7 @@ $root = $_SERVER['DOCUMENT_ROOT'];
 <html>
 
 <head>
-    <title>Houslr</title>
+    <title>Houselr</title>
     <link rel="stylesheet" type="text/css" href="/app/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
@@ -23,13 +23,11 @@ $root = $_SERVER['DOCUMENT_ROOT'];
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
-            <label for="inputPassword5" class="form-label">Password</label>
-            <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
-            <div id="passwordHelpBlock" class="form-text">
+            <div>
+                <label for="inputPassword5" class="form-label">Password</label>
+                <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
             </div>
-            <br>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </main>
