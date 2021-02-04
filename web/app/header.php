729dd75fab2093353d654d1505aa7a2c53dc89fe
mountain <Fiveoat@gmail.com>
2021-02-03 21:28:36 -0700

    x

--- web/app/header.php
+++ web/app/header.php
@@ -1,1 +1,4 @@
 <h1 class="display-1 justify-content-end"><a href="../app/welcome.php"> 🏰 🏠 🏯 Houslr 🏰 🏠 🏯 &ensp;</a></h1>
+<?php
+echo hash('ripemd160', 'The quick brown fox jumped over the lazy dog.');
+?>
