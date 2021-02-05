fbdf940197e8fc5d7ac7fe0e898aaa0cac8a0ada
mountain <Fiveoat@gmail.com>
2021-02-05 00:25:53 -0700

    x

---
+++ web/app/add_booking.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+$root = $_SERVER['DOCUMENT_ROOT'];
+require $root . '/app/connection.php';
+$user_id = (int) $_SESSION['user_id'];
+$home_id = $_GET['home_id'];
+$duration = $_GET['duration'];
+function book_home($home_id, $user_id, $duration){
+         $db = getConnection();
+         $statement = $db->prepare("INSERT INTO bookings (renter, home_id, duration) VALUES (?, ?, ?)");
+         $statement->execute(array($user_id, $home_id, $duration));
+     }
+book_home($home_id, $user_id, $duration);
+header("location: account.php");
+?>
---
+++ web/app/book.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+$root = $_SERVER['DOCUMENT_ROOT'];
+require $root . '/app/connection.php';
+$_POST['home_id'] = $_GET['home_id'];
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Houslr</title>
+    <link rel="stylesheet" type="text/css" href="/app/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+
+<body>
+    <header class="display-4">
+        <?php require $root . '/app/header.php'; ?>
+    </header>
+    <nav>
+        <?php require $root . '/app/nav.php'; ?>
+    </nav>
+    <br>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                    <form method="POST" action="../app/add_booking.php">
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Duration</label>
+                            <select class="form-select" name="duration" aria-label="Default select example">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option value="14">14</option>
+                                <option value="15">15</option>
+                            </select>
+                            <div id="emailHelp" class="form-text">&ensp;Please select the number of nights you wish to stay.</div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Book!</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <?php require $root . '/app/footer.php'; ?>
+    </footer>
+
+</body>
+
+</html>
