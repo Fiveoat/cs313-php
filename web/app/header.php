3e8907254f05e9fd494509a9979ae686090e1080
mountain <Fiveoat@gmail.com>
2021-02-05 01:34:17 -0700

    x

--- web/app/header.php
+++ web/app/header.php
@@ -1,1 +1,1 @@
-&ensp;&ensp;<h1 class="display-1 justify-content-end btn btn-outline-secondary"><a href="../app/welcome.php"> 🏰 🏠 🏯 Houslr 🏰 🏠 🏯 &ensp;</a></h1>
+&ensp;&ensp;<h1 class="display-1 justify-content-end btn btn-outline-secondary center"><a href="../app/welcome.php"> 🏰 🏠 🏯 Houslr 🏰 🏠 🏯 &ensp;</a></h1>
