4c89e159b53090f623793a455a7c48ede2f43b84
mountain <Fiveoat@gmail.com>
2021-02-11 20:20:14 -0700

    x

--- web/app/add_booking.php
+++ web/app/add_booking.php
@@ -4,14 +4,14 @@ $root = $_SERVER['DOCUMENT_ROOT'];
 require $root . '/app/connection.php';
 if (isset($_SESSION['user_id'])){
     $user_id = (int) $_SESSION['user_id'];
-$home_id = $_POST['home_id'];
+$home_id = (int) $_POST['home_id'];
 $duration = (int) $_POST['duration'];
 echo $home_id;
 echo $duration;
 echo $user_id;
 function book_home($home_id, $user_id, $duration){
          $db = getConnection();
-         $statement = $db->prepare("INSERT INTO bookings (renter, home_id, duration, booked) VALUES (?, ?, ?, ?)");
+         $statement = $db->prepare("INSERT INTO bookings (renter, home_id, duration, booked) VALUES (?, ?, ?, ?, ?)");
          $statement->execute(array($user_id, $home_id, $duration, 1));
      }
 book_home($home_id, $user_id, $duration);
