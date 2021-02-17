5dcadade47ab316ce977769faf07b10ed16b631b
mountain <Fiveoat@gmail.com>
2021-02-16 19:41:06 -0700

    x

--- web/app/header.php
+++ web/app/header.php
@@ -1,1 +1,1 @@
-&ensp;<img src="../images/houslr.png" width="100px"><a href="../app/welcome.php"></a></img>
+&ensp;<img class='logo' src="../images/houslr.png" width="100px"><a href="../app/welcome.php"></a></img>
